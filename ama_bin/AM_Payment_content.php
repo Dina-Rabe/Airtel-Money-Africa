a64aa88146ef1d41695429f5bb8e0fc390405e68
Dina Rabenarimanitra <dina@allo-win.net>
2024-01-19 18:28:47 +0300

    Segregation

--- ama_bin/AM_Payment_content.php
+++ ama_bin/AM_Payment_content.php
@@ -2,33 +2,20 @@
 
 class AMA_Payment_Page_Content {
     private $title;
-    private $description;
+    private $content;
 
-    public function __construct($title, $description) {
+    public function __construct($title, $content) {
         $this->title = $title;
-        $this->description = $description;
+        $this->content = $content;
+    }  
+
+    public function getTitle() {
+        return $this->title;
     }
 
-    public function get_content_html() {
-        $msisdn = array(
-            'type' => 'tel',
-            'name' => 'msisdn',
-            'min' => '0',
-            'step' => '1',
-            'required' => true
-        );
-        $submitButton = 'Do Payment';
-        $msisdnLabel = 'MSISDN';
-        $msisdnPlaceholder = '123456789';
-
-        $content_html = '<h1>Payment over ' . $this->title . '</h1>
-                         <p> You will do a payment of' . $this->description . '</p>
-                         <form method="post">
-                             <label for="' . $msisdn['name'] . '">' . $msisdnLabel . '</label>
-                             <input type="' . $msisdn['type'] . '" name="' . $msisdn['name'] . '" min="' . $msisdn['min'] . '" step="' . $msisdn['step'] . '" placeholder="' . $msisdnPlaceholder . '" ' . ($msisdn['required'] ? 'required' : '') . '>
-                             <button type="submit">' . $submitButton . '</button>
-                         </form>';
-
-        return $content_html;
+    public function getContent() {
+        return $this->content;
     }
+    
+
 }
